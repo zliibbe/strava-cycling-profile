af99b79b3cf7b39e263af0cb5580558845f6e015
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2025-09-19 14:28:08 -0600

    test(frontend): add React Testing Library and Jest configuration

---
+++ frontend/src/types/css-modules.d.ts
@@ -0,0 +1,30 @@
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+// Also handle non-module CSS files
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: any;
+  export default content;
+}
